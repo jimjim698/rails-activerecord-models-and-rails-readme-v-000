75e5f5c3d5f0c6b4aa3495f8ab0b03491b98cb90
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-16 01:02:36 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,4 @@ describe Post do
   post = Post.create!(title: "My title", description: "The post description")
   expect(post.post_summary).to eq("My title - The post description")
 end
-end 
+end
